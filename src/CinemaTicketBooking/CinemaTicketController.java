d4d26c1859ef277bb89b017bcc082fb13f70d51d
A.M.S.N.Bandara <shanukaalahakoon456@gmail.com>
2024-12-24 23:28:49 +0530

    Initial commit

---
+++ src/CinemaTicketBooking/CinemaTicketController.java
@@ -0,0 +1,56 @@
+package CinemaTicketBooking;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class CinemaTicketController {
+
+    @FXML
+    private Button signin_close;
+
+    @FXML
+    private Hyperlink signin_createAccount;
+
+    @FXML
+    private AnchorPane signin_form;
+
+    @FXML
+    private Button signin_loginbtn;
+
+    @FXML
+    private Button signin_minus;
+
+    @FXML
+    private TextField signin_password;
+
+    @FXML
+    private TextField signin_userName;
+
+    @FXML
+    private Hyperlink signup_alreadyHave;
+
+    @FXML
+    private Button signup_btn;
+
+    @FXML
+    private Button signup_close;
+
+    @FXML
+    private TextField signup_email;
+
+    @FXML
+    private AnchorPane signup_form;
+
+    @FXML
+    private Button signup_minus;
+
+    @FXML
+    private TextField signup_password;
+
+    @FXML
+    private TextField signup_userName;
+
+}
